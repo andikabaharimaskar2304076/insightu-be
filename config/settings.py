d560c42ea7c262cb427670eaabd27c525437987f
AndikaMaskar <andika7496@gmail.com>
2025-05-10 07:27:44 +0700

    Fixing Migrations Bug

--- config/settings.py
+++ config/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.users',
+    'api.counseling',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.users',
-    'api.counseling',
     'rest_framework'
 ]
 
