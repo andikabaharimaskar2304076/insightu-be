33a11ff8904f1a3bcb8bc280b5d3776d247f3b1a
mSyukri-upi <m.syukri@upi.edu>
2025-05-10 19:36:54 +0700

    memasukkan models dari app 'users' dan app 'counseling' ke admin.py

--- api/counseling/admin.py
+++ api/counseling/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+app = apps.get_app_config('counseling')
+
+for model in app.get_models():
+    admin.site.register(model)
+
+app = apps.get_app_config('users')
+
+for model in app.get_models():
+    admin.site.register(model)
